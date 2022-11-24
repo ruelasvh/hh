014ad90070700cd597c65816d3b5690c93226418
Victor Hugo Ruelas Rivera <victor.hugo.ruelas.rivera@cern.ch>
2022-11-24 13:14:59 +0100

    Prepare for GHH4b samples

--- plotting_match_bosons.py
+++ plotting_match_bosons.py
@@ -8,17 +8,19 @@ from matplotlib.figure import Figure
 import hep2plts as plts
 
 
-X1000_S100_mc20a = "/lustre/fs22/group/atlas/ruelasv/hh4b-analysis-r22-build/samples/mc20_13TeV.hh4b_boosted_via_sh4b_boosted/grid/output/user.viruelas.HH4b-no-trig.2022_10_20.801595.Py8EG_A14NNPDF23LO_XHS_X1000_S100_4b.e8448_a899_r13167_p5057_TREE/user.viruelas.30932128._000001.output-hh4b.root"
+X1000_S100_mc20a = "/lustre/fs22/group/atlas/ruelasv/samples/mc20_13TeV.hh4b_boosted_via_sh4b_boosted/grid/output/user.viruelas.HH4b-no-trig.2022_10_20.801595.Py8EG_A14NNPDF23LO_XHS_X1000_S100_4b.e8448_a899_r13167_p5057_TREE/user.viruelas.30932128._000001.output-hh4b.root"
 
-X1000_S100_mc20d = "/lustre/fs22/group/atlas/ruelasv/hh4b-analysis-r22-build/samples/mc20_13TeV.hh4b_boosted_via_sh4b_boosted/grid/output/user.viruelas.HH4b-no-trig.2022_10_20.801595.Py8EG_A14NNPDF23LO_XHS_X1000_S100_4b.e8448_a899_r13144_p5057_TREE/user.viruelas.30932122._000001.output-hh4b.root"
+X1000_S100_mc20d = "/lustre/fs22/group/atlas/ruelasv/samples/mc20_13TeV.hh4b_boosted_via_sh4b_boosted/grid/output/user.viruelas.HH4b-no-trig.2022_10_20.801595.Py8EG_A14NNPDF23LO_XHS_X1000_S100_4b.e8448_a899_r13144_p5057_TREE/user.viruelas.30932122._000001.output-hh4b.root"
 
-X1000_S100_mc20e = "/lustre/fs22/group/atlas/ruelasv/hh4b-analysis-r22-build/samples/mc20_13TeV.hh4b_boosted_via_sh4b_boosted/grid/output/user.viruelas.HH4b-no-trig.2022_10_20.801595.Py8EG_A14NNPDF23LO_XHS_X1000_S100_4b.e8448_a899_r13145_p5057_TREE/user.viruelas.30932125._000001.output-hh4b.root"
+X1000_S100_mc20e = "/lustre/fs22/group/atlas/ruelasv/samples/mc20_13TeV.hh4b_boosted_via_sh4b_boosted/grid/output/user.viruelas.HH4b-no-trig.2022_10_20.801595.Py8EG_A14NNPDF23LO_XHS_X1000_S100_4b.e8448_a899_r13145_p5057_TREE/user.viruelas.30932125._000001.output-hh4b.root"
 
-X2000_S100_mc20a = "/lustre/fs22/group/atlas/ruelasv/hh4b-analysis-r22-build/samples/mc20_13TeV.hh4b_boosted_via_sh4b_boosted/grid/output/user.viruelas.HH4b-no-trig.2022_10_20.801614.Py8EG_A14NNPDF23LO_XHS_X2000_S100_4b.e8448_a899_r13167_p5057_TREE/user.viruelas.30932131._000001.output-hh4b.root"
+X2000_S100_mc20a = "/lustre/fs22/group/atlas/ruelasv/samples/mc20_13TeV.hh4b_boosted_via_sh4b_boosted/grid/output/user.viruelas.HH4b-no-trig.2022_10_20.801614.Py8EG_A14NNPDF23LO_XHS_X2000_S100_4b.e8448_a899_r13167_p5057_TREE/user.viruelas.30932131._000001.output-hh4b.root"
 
-X3000_S100_mc20a = "/lustre/fs22/group/atlas/ruelasv/hh4b-analysis-r22-build/samples/mc20_13TeV.hh4b_boosted_via_sh4b_boosted/grid/output/user.viruelas.HH4b-no-trig.2022_10_20.801637.Py8EG_A14NNPDF23LO_XHS_X3000_S100_4b.e8448_a899_r13167_p5057_TREE/user.viruelas.30932136._000001.output-hh4b.root"
+X3000_S100_mc20a = "/lustre/fs22/group/atlas/ruelasv/samples/mc20_13TeV.hh4b_boosted_via_sh4b_boosted/grid/output/user.viruelas.HH4b-no-trig.2022_10_20.801637.Py8EG_A14NNPDF23LO_XHS_X3000_S100_4b.e8448_a899_r13167_p5057_TREE/user.viruelas.30932136._000001.output-hh4b.root"
 
-X2000_S400_mc20e_TEST = "/lustre/fs22/group/atlas/ruelasv/hh4b-analysis-r22-build/run/analysis-variables.root"
+X2000_S400_mc20e_TEST = "/lustre/fs22/group/atlas/ruelasv/run/analysis-variables.root"
+
+G_HH_bbbb_mc20d = "/lustre/fs22/group/atlas/ruelasv/hh4b-analysis-r22-build/run/analysis-variables.root"
 
 invGeV = 1 / 1000
 
@@ -34,6 +36,7 @@ def draw_leadingVR_jets(data, ax, label=None, title=None, atlas_sec_tag=None):
     ax.hhist(
         jet_counts,
         bins,
+        label,
         title,
         x_label=x_label,
         atlas_sec_tag=atlas_sec_tag,
@@ -42,7 +45,7 @@ def draw_leadingVR_jets(data, ax, label=None, title=None, atlas_sec_tag=None):
 
 def draw_pT_hist(data, ax, label=None, truth=None, title=None, atlas_sec_tag=None):
     # bins = np.linspace(250, 3500, 50)
-    bins = np.linspace(200, 2000, 36)
+    bins = np.linspace(100, 2000, 36)
     jet_counts, _ = np.histogram(data * invGeV, bins)
     x_label = (
         "Leading large-R jet $p_{\mathrm{T}}$ [GeV]"
@@ -62,7 +65,7 @@ def draw_pT_hist(data, ax, label=None, truth=None, title=None, atlas_sec_tag=Non
 
 
 def draw_m_hist(data, ax, label=None, truth=None, title=None, atlas_sec_tag=None):
-    bins = np.linspace(0, 1000, 51)
+    bins = np.linspace(0, 600, 51)
     jet_counts, _ = np.histogram(data * invGeV, bins)
     x_label = f"Leading large-R jet mass [GeV] {f'matched to {truth}' if truth else ''}"
 
@@ -200,12 +203,12 @@ def draw_largeR_jets(largeR_dict):
     fig_truth_matched_to_S_jetPt, ax_truth_matched_to_S_jetPt = plts.subplots()
     fig_VRtrack_jets, ax_VRtrack_jets = plts.subplots()
 
-    pt_cut = 200e3
+    pt_cut = 100e3
     eta_cut = 2
     m_cut = 50e3
-    cuts_label = "$p_{\mathrm{T}}$ > 200 GeV, m > 50 GeV and $|\eta| < 2$"
+    cuts_label = "$p_{\mathrm{T}}$ > 100 GeV, m > 50 GeV and $|\eta| < 2$"
     for sample_name, sample in largeR_dict.items():
-        atlas_sec_tag = f"{sample_name}\n{cuts_label}"
+        atlas_sec_tag = f"DSID 514722, mc20d\n{cuts_label}"
         cuts = (
             (sample["recojet_antikt10_NOSYS_pt"] > pt_cut)
             & (sample["recojet_antikt10_NOSYS_m"] > m_cut)
@@ -214,11 +217,13 @@ def draw_largeR_jets(largeR_dict):
         draw_pT_hist(
             np.concatenate(sample["recojet_antikt10_NOSYS_pt"][cuts]),
             ax_jetPt,
+            sample_name,
             atlas_sec_tag=atlas_sec_tag,
         )
         draw_m_hist(
             np.concatenate(sample["recojet_antikt10_NOSYS_m"][cuts]),
             ax_jetM,
+            sample_name,
             atlas_sec_tag=atlas_sec_tag,
         )
         draw_leadingVR_jets(
@@ -232,40 +237,42 @@ def draw_largeR_jets(largeR_dict):
         draw_deltaR_hist(
             ak.ravel(truth_matched_jets["jet_deltaR_to_H"]),
             ax_deltaR_to_H,
+            sample_name,
             truth="H",
             atlas_sec_tag=atlas_sec_tag,
         )
         draw_deltaR_hist(
             ak.ravel(truth_matched_jets["jet_deltaR_to_S"]),
             ax_deltaR_to_S,
+            sample_name,
             truth="S",
             atlas_sec_tag=atlas_sec_tag,
         )
         draw_m_hist(
             ak.ravel(truth_matched_jets["truth_matched_to_H_jet"].mass),
             ax_truth_matched_to_H_jetM,
-            label="No btag",
+            label=f"{sample_name} No btag",
             truth="H",
             atlas_sec_tag=atlas_sec_tag,
         )
         draw_pT_hist(
             ak.ravel(truth_matched_jets["truth_matched_to_H_jet"].pt),
             ax_truth_matched_to_H_jetPt,
-            label="No btag",
+            label=f"{sample_name} No btag",
             truth="H",
             atlas_sec_tag=atlas_sec_tag,
         )
         draw_m_hist(
             ak.ravel(truth_matched_jets["truth_matched_to_S_jet"].mass),
             ax_truth_matched_to_S_jetM,
-            label="No btag",
+            label=f"{sample_name} No btag",
             truth="S",
             atlas_sec_tag=atlas_sec_tag,
         )
         draw_pT_hist(
             ak.ravel(truth_matched_jets["truth_matched_to_S_jet"].pt),
             ax_truth_matched_to_S_jetPt,
-            label="No btag",
+            label=f"{sample_name} No btag",
             truth="S",
             atlas_sec_tag=atlas_sec_tag,
         )
@@ -274,28 +281,28 @@ def draw_largeR_jets(largeR_dict):
         draw_m_hist(
             ak.ravel(truth_matched_jets_btag["truth_matched_to_H_jet_btag77"].mass),
             ax_truth_matched_to_H_jetM,
-            label="btag77",
+            label=f"{sample_name} btag77",
             truth="H",
             atlas_sec_tag=atlas_sec_tag,
         )
         draw_pT_hist(
             ak.ravel(truth_matched_jets_btag["truth_matched_to_H_jet_btag77"].pt),
             ax_truth_matched_to_H_jetPt,
-            label="btag77",
+            label=f"{sample_name} btag77",
             truth="H",
             atlas_sec_tag=atlas_sec_tag,
         )
         draw_m_hist(
             ak.ravel(truth_matched_jets_btag["truth_matched_to_S_jet_btag77"].mass),
             ax_truth_matched_to_S_jetM,
-            label="btag77",
+            label=f"{sample_name} btag77",
             truth="S",
             atlas_sec_tag=atlas_sec_tag,
         )
         draw_pT_hist(
             ak.ravel(truth_matched_jets_btag["truth_matched_to_S_jet_btag77"].pt),
             ax_truth_matched_to_S_jetPt,
-            label="btag77",
+            label=f"{sample_name} btag77",
             truth="S",
             atlas_sec_tag=atlas_sec_tag,
         )
@@ -334,7 +341,7 @@ def draw_largeR_jets(largeR_dict):
 def run():
     largeR_dict = {}
     samples = {
-        "X2000_S400_mc20e_TEST": X2000_S400_mc20e_TEST,
+        "G_HH_bbbb": G_HH_bbbb_mc20d,
         # "X1000_S100_mc20a": X1000_S100_mc20a,
         # "X2000_S100_mc20a": X2000_S100_mc20a,
         # "X3000_S100_mc20a": X3000_S100_mc20a,
